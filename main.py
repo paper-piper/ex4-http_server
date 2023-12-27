afbd04e188b55f98ba45c85545b30538971a5021
yoni kisho <yonatanreichert@gmail.com>
2023-12-27 17:20:03 +0200

    Everything working, just need final checks and go through everything (need to improve function documentation)

--- main.py
+++ main.py
@@ -1,114 +1,111 @@
-"""
- HTTP Server Shell
- Author: Barak Gonen and Nir Dweck
- Purpose: Provide a basis for Ex. 4
- Note: The code is written in a simple way, without classes, log files or
- other utilities, for educational purpose
- Usage: Fill the missing functions and constants
-"""
-# TO DO: import modules
+import socket
+import re
+import os
 
-# TO DO: set constants
-
+# Constants
 QUEUE_SIZE = 10
 IP = '0.0.0.0'
 PORT = 80
 SOCKET_TIMEOUT = 2
-CONTENT_TYPE_DIC = {"Html": "text/html;charset=utf-8", "jpg": "image/jpeg", "css": "text/css",
-                    "js": "text/javascript; charset=UTF-8",
-                    "txt": "text/plain", "ico": "image/x-icon", "gif": "image/jpeg", "png": "image/png"}
-
-"""
-functions to do:
-getting http request
-validating http request
-breaking up the http request
-handleing the request:
-    getting the right recources according to request
-    reparsing http response
-    sending the response
-"""
+CONTENT_TYPES = {
+    "html": "text/html;charset=utf-8",
+    "jpg": "image/jpeg",
+    "css": "text/css",
+    "js": "text/javascript; charset=UTF-8",
+    "txt": "text/plain",
+    "ico": "image/x-icon",
+    "gif": "image/gif",
+    "png": "image/png"
+}
+WEBROOT = "webroot"
+DEFAULT_URL = WEBROOT + "/index.html"
 
 
 def get_file_data(file_name):
-    """
-    Get data from file
-    :param file_name: the name of the file
-    :return: the file data in a string
-    """
+    """Get data from file."""
+    try:
+        with open(file_name, 'rb') as file:
+            return file.read()
+    except FileNotFoundError:
+        return None
 
 
 def handle_client_request(resource, client_socket):
-    """
-    Check the required resource, generate proper HTTP response and send
-    to client
-    :param resource: the required resource
-    :param client_socket: a socket for the communication with the client
-    :return: None
-    """
-    """ """
-    # TO DO : add code that given a resource (URL and parameters) generates
-    # the proper response
-    if resource == '':
-        uri = DEFAULT_URL
+    """Handle client request."""
+    if resource == '/':
+        url = DEFAULT_URL
     else:
-        uri = resource
+        url = WEBROOT + resource
 
-    # TO DO: check if URL had been redirected, not available or other error
-    # code. For example:
-    if url in REDIRECTION_DICTIONARY:
-        pass
-        # TO DO: send 302 redirection response
+    # Special cases handling
+    if resource == '/forbidden':
+        http_header = "HTTP/1.1 403 Forbidden\r\n\r\n"
+        client_socket.send(http_header.encode())
+        return
+    elif resource == '/moved':
+        http_header = "HTTP/1.1 302 Moved Temporarily\r\nLocation: /\r\n\r\n"
+        client_socket.send(http_header.encode())
+        return
+    elif resource == '/error':
+        http_header = "HTTP/1.1 500 Internal Server Error\r\n\r\n"
+        client_socket.send(http_header.encode())
+        return
+    # Extract the file type (extension)
+    file_type = url.split('.')[-1]
 
-    # TO DO: extract requested file tupe from URL (html, jpg etc)
-    if file_type == 'html':
-        http_header =  # TO DO: generate proper HTTP header
-    elif file_type == 'jpg':
-        http_header =  # TO DO: generate proper jpg header
-    # TO DO: handle all other headers
+    # Check if file exists and is not a directory
+    if not os.path.isfile(url):
+        http_header = "HTTP/1.1 404 Not Found\r\n\r\n"
+        http_response = http_header.encode()
+        client_socket.send(http_response)
+        return
 
-    # TO DO: read the data from the file
-    data = get_file_data(filename)
-    # http_header should be encoded before sended
-    # data encoding depends on its content. text should be encoded, while files shouldn't
-    http_response = http_header.encode() + data
+    # Read the data from the file
+    data = get_file_data(url)
+
+    if data:
+        http_header = f"HTTP/1.1 200 OK\r\nContent-Type: {CONTENT_TYPES.get(file_type, 'text/plain')}\r\n\r\n"
+        http_response = http_header.encode() + data
+    else:
+        http_header = "HTTP/1.1 404 Not Found\r\n\r\n"
+        http_response = http_header.encode()
+
     client_socket.send(http_response)
 
 
-def validate_http_request(request):
-    """
-    Check if request is a valid HTTP request and returns TRUE / FALSE and
-    the requested URL
-    :param request: the request which was received from the client
-    :return: a tuple of (True/False - depending if the request is valid,
-    the requested resource )
-    """
-    # TO DO: write function
+def parse_http_request(request):
+    """Parse HTTP request."""
+    pattern = re.compile(r'^(GET)\s+(/[^ ]*)\s+HTTP/1\.1\r\n')
+    match = pattern.match(request.decode())
+
+    if match:
+        method, requested_url = match.groups()
+        return True, requested_url
+    else:
+        return False, None
 
 
 def handle_client(client_socket):
-    """
-    Handles client requests: verifies client's requests are legal HTTP, calls
-    function to handle the requests
-    :param client_socket: the socket for the communication with the client
-    :return: None
-    """
+    """Handle client."""
     print('Client connected')
-    while True:
-        # TO DO: insert code that receives client request
-        # ...
-        valid_http, resource = validate_http_request(client_request)
+    try:
+        client_request = client_socket.recv(1024)
+
+        valid_http, resource = parse_http_request(client_request)
         if valid_http:
             print('Got a valid HTTP request')
             handle_client_request(resource, client_socket)
         else:
             print('Error: Not a valid HTTP request')
-            break
+            http_header = "HTTP/1.1 400 Bad Request\r\n\r\n"
+            client_socket.send(http_header.encode())
+    except socket.timeout:
+        print('Client request timed out')
     print('Closing connection')
 
 
 def main():
-    # Open a socket and loop forever while waiting for clients
+    """Main function."""
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         server_socket.bind((IP, PORT))
@@ -118,19 +115,21 @@ def main():
         while True:
             client_socket, client_address = server_socket.accept()
             try:
-                print('New connection received')
                 client_socket.settimeout(SOCKET_TIMEOUT)
                 handle_client(client_socket)
             except socket.error as err:
-                print('received socket exception - ' + str(err))
+                print('Received socket exception - ' + str(err))
             finally:
                 client_socket.close()
     except socket.error as err:
-        print('received socket exception - ' + str(err))
+        print('Received socket exception - ' + str(err))
     finally:
         server_socket.close()
 
 
 if __name__ == "__main__":
-    # Call the main handler function
+    # some assertion checks
+    assert parse_http_request(b"GET / HTTP/1.1\r\n")[0]
+    assert not parse_http_request(b"GET /not_a_real_page HTTP/1.0\r\n")[0]
+    assert not parse_http_request(b"BADREQUEST / HTTP/1.1\r\n")[0]
     main()
